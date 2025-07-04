0c374ef7c7957435212501ec264b4c7cc7fa5f39
ObeseTomato <golarzluca@gmail.com>
2025-07-04 19:36:57 -0400

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,23 +8,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-        configure: (proxy, _options) => {
-          proxy.on('error', (err, _req, _res) => {
-            console.log('proxy error', err);
-          });
-          proxy.on('proxyReq', (proxyReq, req, _res) => {
-            console.log('Sending Request to the Target:', req.method, req.url);
-          });
-          proxy.on('proxyRes', (proxyRes, req, _res) => {
-            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
-          });
-        },
-      }
-    }
+    // The proxy section has been removed
   },
   plugins: [
     react(),
